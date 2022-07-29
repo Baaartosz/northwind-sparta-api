30d5bd5bc8c3a5c3823ce8abb17451c7670283cf
OTDZ <omartehami6@gmail.com>
2022-07-29 17:31:34 +0100

    Implemented Post test (not working yet)

--- src/test/java/com/sparta/group3/northwindtask/northwindtask/OrderTests.java
+++ src/test/java/com/sparta/group3/northwindtask/northwindtask/OrderTests.java
@@ -15,7 +15,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
 
 @SpringBootTest()
 public class OrderTests {
@@ -91,6 +96,59 @@ public class OrderTests {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    @DisplayName("Add order")
+    void addOrder() {
+        if (orderRepository.existsById(11100)) {
+            orderRepository.deleteById(11100);
+        }
+
+        //______________________________________BODY MAKER_________________
+        var values = new HashMap<String, String>() {{
+            put("id", "11100");
+            put("customerID", "'BRAND'");
+            put("employeeID", "8");
+            put("orderDate", "1996-07-22 00:00:00");
+            put("requiredDate", "1996-08-19 00:00:00");
+            put("shippedDate", "1996-07-25 00:00:00");
+            put("shipVia", "3");
+            put("freight", "48.2900");
+            put("shipName", "Rattlesnake Canyon Grocery");
+            put("shipAddress", "2817 Milton Dr.");
+            put("shipCity", "Chicago");
+            put("shipRegion", "NM");
+            put("shipPostalCode", "'87110'");
+            put("shipCountry", "USA");
+        }};
+
+        try {
+            String requestBody = mapper.writeValueAsString(values);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(serverURL + "/orders/new")) //CHANGE THE LOCATION TO SEND BODY TO
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                    .build();
+
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        //____________________________________END OF BODY MAKER___________________________
+
+//        try {
+//            Customer result = mapper.readValue(
+//                    new URL("http://localhost/customers/id/AAAAA"),
+//                    Customer.class);
+//            Assertions.assertNotEquals(null, result);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } // VVVVVVVVVVVVV
+        Assertions.assertNotEquals(null, orderRepository.findById(11100).get());
+        orderRepository.deleteById(11100);
     }
 
 }
